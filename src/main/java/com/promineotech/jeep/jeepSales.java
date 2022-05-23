650e5b0cb90ce8a5b44e8e2b3345c3573eff625e
Jessica Smiejan <aniline702@yahoo.com>
2022-05-23 15:16:47 -0700

    first attempt

---
+++ src/main/java/com/promineotech/jeep/jeepSales.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author anili
+ *
+ */
+@SpringBootApplication
+public class jeepSales {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(jeepSales.class, args);
+  }
+
+}
---
+++ src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.controller.support;
+
+/**
+ * @author anili
+ *
+ */
+public class FetchJeepTestSupport  extends BaseTest{
+
+}
